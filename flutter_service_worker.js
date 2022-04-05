487f7748af41face1ba84fda706c3440090b8a30
MahanRahmati <mahan.rahmati@protonmail.com>
2022-04-05 04:51:17 +0430

    0.3.6

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "7f8dc62c1f4d5bd242487095a468ef87",
-"index.html": "c69f239f6e0404223e231aeddf8f0857",
-"/": "c69f239f6e0404223e231aeddf8f0857",
-"main.dart.js": "fd45601471f8ed1c97031c9781b6ed44",
+"index.html": "d623e73954349ca5256afc15327098a3",
+"/": "d623e73954349ca5256afc15327098a3",
+"main.dart.js": "40dbf394b29f4e2cf8452a1b4fb93c77",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
